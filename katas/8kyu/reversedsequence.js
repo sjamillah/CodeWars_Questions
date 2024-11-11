6df328996b85b46968104fccf009ac7f35aff098
sjamillah <j.ssozi@alustudent.com>
2024-11-11 18:13:21 +0200

    added comments on the function expression

--- katas/8kyu/reversedsequence.js
+++ katas/8kyu/reversedsequence.js
@@ -1,4 +1,5 @@
 const reverseSeq = n => {
+  //reverses a sequence based on the entered value so it starts from that  value to the least value
     let result = [];
     for (var i = n; i > 0; i--) { //moves through indices equal to the number of characters
       result.push(i); //adds the elements to the array
