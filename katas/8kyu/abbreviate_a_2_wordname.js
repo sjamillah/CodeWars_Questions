c094d8eb56485d69a8766457e2fb09a2f9b10e52
sjamillah <j.ssozi@alustudent.com>
2024-11-19 17:03:17 +0200

    made some changes

--- katas/8kyu/abbreviate_a_2_wordname.js
+++ katas/8kyu/abbreviate_a_2_wordname.js
@@ -1,4 +1,5 @@
 function abbrevName(name) {
+    /*abbreviates a name with the first letter as initials and puts a dot in the middle*/
     let sStr = name.split(' ');
     let firstInitial = sStr[0].charAt(0).toUpperCase();
     let secondInitial = sStr[1].charAt(0).toUpperCase();
